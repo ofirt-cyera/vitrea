870bacc94843ce8bfc2b6c8fb7e48ffd5417084d
Ofir Talmor <ofirt@cyera.io>
2025-03-29 15:56:37 +0300

    Install vitrea-client.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, Node.js + TypeScript!");
+import {
+    ProtocolVersion,
+    Requests,
+    VitreaClient
+} from 'vitrea-client'
+
+
+const client = VitreaClient.create({
+    host:     '192.168.1.136',
+    port:     11503,
+    username: 'vitrea1',
+    password: 'ps1',
+})
+
+await client.connect()
+
+const count = await client.send(new Requests.RoomCount())
